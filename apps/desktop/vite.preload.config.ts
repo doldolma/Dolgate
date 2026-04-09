ee7d3ddfe0989bb8764feec8271a757c302ba2a7
dolma <dolma@gridwiz.com>
2026-04-09 12:59:17 +0900

    update github workflow

--- apps/desktop/vite.preload.config.ts
+++ apps/desktop/vite.preload.config.ts
@@ -1,4 +1,9 @@
 import { mergeConfig } from 'vite';
 import baseConfig from './vite.base.config';
 
-export default mergeConfig(baseConfig, {});
+export default mergeConfig(baseConfig, {
+  test: {
+    environment: 'node',
+    include: ['src/preload/**/*.test.ts']
+  }
+});
